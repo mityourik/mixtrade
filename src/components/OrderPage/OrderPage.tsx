a3b05134911927c9295aff1319428e3da19e6796
Dmitry <mityourik@gmail.com>
2024-11-24 00:33:33 +0200

    add order-page layout

--- src/components/OrderPage/OrderPage.tsx
+++ src/components/OrderPage/OrderPage.tsx
@@ -1,32 +1,45 @@
 import styles from './OrderPage.module.scss';
-import stationXl from '../../vendor/images/BM-670.png';
+import stationXl from '../../vendor/images/xl.png';
 
 const OrderPage: React.FC = () => {
   return (
     <section className={styles['order-page']}>
       <div className={styles['order-page__container']}>
-        <div className={styles['order-page__image-wrapper']}>
-          <img className={styles['order-page__image']} src={stationXl} alt="Штукатурная станция XL" />
+        <div className={styles['order-page__header']}>
+          <h1 className={styles['order-page__title']}>XL</h1>
+          <h2 className={styles['order-page__subtitle']}>Штукатурная станция</h2>
+        </div>
+        <div className={styles['order-page__image-container']}>
+          <img
+            className={styles['order-page__image']}
+            src={stationXl}
+            alt="Штукатурная станция XL"
+          />
         </div>
         <div className={styles['order-page__content']}>
-          <h1 className={styles['order-page__title']}>BM 670</h1>
-          <p className={styles['order-page__text']}>
-            Бетоносмеситель PERSONIYA БМ 670 служит для замешивания растворов известки и штукатурки, бетонных смесей и сухих материалов при выполнении строительных и ремонтных работ. Оснащена надежной, съемной колесной базой. Бетоносмеситель укомплектован съемным лотком для удобной выгрузки готовой продукции, что значительно упрощает ее эксплуатацию.
+          <p className={styles['order-page__description']}>
+            PERSONIYA XL - штукатурная станция, предназначенная для качественного и быстрого
+            нанесения сухих строительных смесей. Может работать от сети с трёхфазным током 380V,
+            если это невозможно, подойдёт и 220V. Штукатурная станция справляется с ремонтными
+            работами, как на частных объектах, так и на масштабных строительных площадках.
           </p>
           <div className={styles['order-page__specs']}>
-            <h4 className={styles['order-page__spec-title']}>Характеристики:</h4>
-            <ul className={styles['order-page__spec-list']}>
-              <li>Электропитание - 220V</li>
-              <li>Мощность - 3 кВт</li>
-              <li>Вес - 140 кг</li>
-              <li>Число оборотов - 378-400 об/мин.</li>
-              <li>Производительность - 1 м³/ч.</li>
-              <li>Объем бака - 80 л.</li>
+            <h3 className={styles['order-page__specs-title']}>Характеристики:</h3>
+            <ul className={styles['order-page__specs-list']}>
+              <li className={styles['order-page__specs-item']}>Электропитание: 220/380 V</li>
+              <li className={styles['order-page__specs-item']}>Дальность подачи: до 15 м</li>
+              <li className={styles['order-page__specs-item']}>Высота подачи: 10 м</li>
+              <li className={styles['order-page__specs-item']}>Вес: 246 кг</li>
+              <li className={styles['order-page__specs-item']}>Число оборотов: 50-390 об/мин</li>
+              <li className={styles['order-page__specs-item']}>Производительность: 8-20 л/мин</li>
+              <li className={styles['order-page__specs-item']}>Объем приёмного бункера: 70 л</li>
             </ul>
           </div>
           <p className={styles['order-page__warranty']}>Гарантия: 14 месяцев</p>
-          <p className={styles['order-page__price']}>105000 Р.</p>
-          <button className={styles['order-page__buy-button']}>Купить</button>
+          <div className={styles['order-page__footer']}>
+            <span className={styles['order-page__price']}>600000 Р.</span>
+            <button className={styles['order-page__buy-button']}>Купить</button>
+          </div>
         </div>
       </div>
     </section>
