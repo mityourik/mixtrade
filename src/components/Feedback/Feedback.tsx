0c21c42612759f1f9bbc02ecac7d00ee360b5aca
Dmitry <mityourik@gmail.com>
2024-11-25 09:03:19 +0200

    add contact info to feedback

--- src/components/Feedback/Feedback.tsx
+++ src/components/Feedback/Feedback.tsx
@@ -1,65 +1,3 @@
-// import styles from './Feedback.module.scss';
-
-// const Feedback: React.FC = () => {
-//   return (
-//     // <section className={`${styles.feedback} scroll-section`}>
-//     <section className={styles.feedback}>
-//       <div className={styles.feedback__container}>
-//         <div className={styles.feedback__form}>
-//           <h1 className={styles.feedback__title}>обратная связь</h1>
-//           <p className={styles.feedback__description}>
-//             Здесь вы можете задать интересующие Вас вопросы.
-//           </p>
-//           <form>
-//             <input
-//               type="text"
-//               className={styles.feedback__input}
-//               placeholder="Имя"
-//             />
-//             <input
-//               type="email"
-//               className={styles.feedback__input}
-//               placeholder="E-mail"
-//             />
-//             <input
-//               type="tel"
-//               className={styles.feedback__input}
-//               placeholder="Телефон"
-//             />
-//             <textarea
-//               className={styles.feedback__textarea}
-//               placeholder="Ваш вопрос"
-//             ></textarea>
-//             <button className={styles.feedback__submit} type="submit">
-//               Отправить
-//             </button>
-//           </form>
-//         </div>
-
-//         <div className={styles.feedback__info}>
-//           <p className={styles.feedback__text}>
-//             Чтобы не ждать ответа, звоните по телефону или переходите в удобный
-//             для Вас мессенджер.
-//           </p>
-//           <div className={styles.feedback__icons}>
-//             <div
-//               className={`${styles.feedback__icon} ${styles['feedback__icon__telegram']}`}
-//             ></div>
-//             <div
-//               className={`${styles.feedback__icon} ${styles['feedback__icon__whatsapp']}`}
-//             ></div>
-//             <div
-//               className={`${styles.feedback__icon} ${styles['feedback__icon__phone']}`}
-//             ></div>
-//           </div>
-//         </div>
-//       </div>
-//     </section>
-//   );
-// };
-
-// export default Feedback;
-
 import styles from './Feedback.module.scss';
 
 const Feedback: React.FC = () => {
@@ -104,16 +42,28 @@ const Feedback: React.FC = () => {
             Чтобы не ждать ответа, звоните по телефону или переходите в удобный
             для Вас мессенджер.
           </p>
-          <div className={styles.feedback__icons}>
-            <div
-              className={`${styles.feedback__icon} ${styles['feedback__icon--telegram']}`}
-            ></div>
-            <div
-              className={`${styles.feedback__icon} ${styles['feedback__icon--whatsapp']}`}
-            ></div>
-            <div
-              className={`${styles.feedback__icon} ${styles['feedback__icon--phone']}`}
-            ></div>
+          <div className={styles.feedback__contacts}>
+            <p className={styles.feedback__phone}>
+              <a href="tel:+79620286161">+7 962 028-61-61</a> служба поддержки
+            </p>
+            <div className={styles.feedback__icons}>
+              <a
+                href="https://t.me/+l601BTBDSgNmODhi"
+                className={`${styles.feedback__icon} ${styles['feedback__icon__telegram']}`}
+                target="_blank"
+                rel="noopener noreferrer"
+              ></a>
+              <a
+                href="https://vk.com/novoplastering"
+                className={`${styles.feedback__icon} ${styles['feedback__icon__vk']}`}
+                target="_blank"
+                rel="noopener noreferrer"
+              ></a>
+              <a
+                href="tel:+79620286161"
+                className={`${styles.feedback__icon} ${styles['feedback__icon__phone']}`}
+              ></a>
+            </div>
           </div>
         </div>
       </div>
